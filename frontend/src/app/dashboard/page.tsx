3bc2b5b768d628caeae7eb98f05a1afdc66ae714
pkemani06 <135028367+pkemani06@users.noreply.github.com>
2025-12-23 18:09:41 -0500

    Navbar removed

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -5,27 +5,7 @@ export default function Dashboard() {
     <div className="min-h-screen bg-white flex justify-center pt-20">
       {/* PAGE WRAPPER */}
       <div className="w-full">
-        {/* NAVBAR */}
-        <div className="bg-white shadow-xl flex justify-center h-15 w-full -mt-20 flex items-center justify-start ">
-          {/* Blue box with "AI" text */}
-          <div className="bg-blue-600 ml-4 rounded-lg w-12 h-9 flex items-center justify-center">
-            <span className="text-white font-bold text-sm">AI</span>
-          </div>
-
-          <span className="ml-2 text-lg font-bold text-blue-600 ">
-            InterviewAI
-          </span>
-          <span className="ml-20 text-xs text-blue-600 ">
-            Dashboard
-          </span>
-          <span className="ml-20  text-xs text-black ">
-            Practice
-          </span>
-          <span className="ml-20  text-xs text-black ">
-            Analytics
-          </span>
-        </div>
-
+       
         {/*Box at the top of the page that says Welcome */}
         <div className="bg-while shadow-xl rounded-xl flex justify-center h-50 w-250 flex flex-col items-left ml-[250px] justify-start p-6 space-y-4 ">
           <div className="text-xl font-bold text-black text-left mt-2">
