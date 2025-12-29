5fe2d8fcaa6e8e04c9a947f3d8b9cd1dd370a39c
pkemani06 <135028367+pkemani06@users.noreply.github.com>
2025-12-29 15:37:20 -0500

    Backend completed with user signup authentication and validation aswell as database integreation

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes';
+
+// read the env file
+dotenv.config()
+// create the expres app
+const app = express()
+
+// define the port we'll be running on
+const PORT = 5000
+
+// middlewear are fxn that run before the routes
+app.use(cors()) // allows the portend to make requests to the backend while they are on diff ports
+app.use(express.json()) // parses the json from requests body›
+
+// register the auth routes give them all a starting name
+// sign up url will be /api/auth/signup
+app.use('/api/auth', authRoutes)
+
+// check if backend is running
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', message: 'Backend is running!' });
+});
+
+// start the server and make it listen on PORT 5000
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Health check: http://localhost:${PORT}/health`);
+  console.log(`Auth endpoints:`);
+  console.log(`POST http://localhost:${PORT}/api/auth/signup`);
+});
