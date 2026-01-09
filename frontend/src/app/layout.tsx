988686fce87f2bda42ff464499bf008f08215567
Fadlulah Omitogun <du33691@umbc.edu>
2026-01-09 04:32:55 -0500

    slight modifications to the accounts and layout page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 import Footer from "./components/Footer";
 import { ThemeProvider } from "./context/ThemeContext";
 
